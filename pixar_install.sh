af8c408bec5ee84b79c57da0b7edd3ceeba58d2b
Hegyi Szabolcs <92859143+ketabruce@users.noreply.github.com>
2025-08-05 00:03:23 +0200

    Update pixar_install.sh
    minor changes for AWS

--- pixar_install.sh
+++ pixar_install.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# Download source from GitHub
-wget https://github.com/ketabruce/pixar/archive/refs/heads/main.zip
-
-# Unzip source code
-unzip main.zip
-
-# Change to code directory
-cd pixar-main
-
 # Install python packages
-pip install dbt-core dbt-sqlserver
-pip install dagster dagster-cloud dagster-dbt dagster-webserver
+pipx install dbt-core dbt-sqlserver
+pipx install dagster dagster-cloud dagster-dbt dagster-webserver
 
 # Install MSSQL ODBC drives
 sh ./odbc_install.sh
