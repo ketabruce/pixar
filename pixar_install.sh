f4e514b1454ee7b6b9f7cfc3457deb6c9c88e2fd
Hegyi Szabolcs <92859143+ketabruce@users.noreply.github.com>
2025-08-05 07:49:33 +0200

    Update pixar_install.sh
    python virtual environment created

--- pixar_install.sh
+++ pixar_install.sh
@@ -4,11 +4,12 @@
 sh ./odbc_install.sh
 
 # Install python packages
-pipx ensurepath
-pipx install --include-deps dbt-core dbt-sqlserver
-pipx install --include-deps dagster dagster-cloud dagster-dbt dagster-webserver
+python3 -m venv /tmp
+export PATH=$PATH:/tmp/bin
+/tmp/bin/pip install --include-deps dbt-core dbt-sqlserver
+/tmp/bin/pip install --include-deps dagster dagster-cloud dagster-dbt dagster-webserver
 
 # Change to Dagster directory
 cd dagster_pixar
 # Start Dagster webserver in background
-dagster dev -h 0.0.0.0 -p 3000 &
+/tmp/bin/dagster dev -h 0.0.0.0 -p 3000 &
