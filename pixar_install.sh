aa19c8e3ff5ee23ccc8232a5a623b9594af4204a
Hegyi Szabolcs <92859143+ketabruce@users.noreply.github.com>
2025-08-05 07:54:05 +0200

    Update pixar_install.sh
    pip flags removed

--- pixar_install.sh
+++ pixar_install.sh
@@ -6,10 +6,10 @@ sh ./odbc_install.sh
 # Install python packages
 python3 -m venv /tmp
 export PATH=$PATH:/tmp/bin
-/tmp/bin/pip install --include-deps dbt-core dbt-sqlserver
-/tmp/bin/pip install --include-deps dagster dagster-cloud dagster-dbt dagster-webserver
+/tmp/bin/pip install dbt-core dbt-sqlserver
+/tmp/bin/pip install dagster dagster-cloud dagster-dbt dagster-webserver
 
 # Change to Dagster directory
 cd dagster_pixar
 # Start Dagster webserver in background
-/tmp/bin/dagster dev -h 0.0.0.0 -p 3000 &
+/tmp/bin/dagster dev -h 0.0.0.0 -p 3000
